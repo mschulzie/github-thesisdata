4df62fb9aec9241b9ad16d78310540304d9102b1
Handballgott <77458288+Handballgott@users.noreply.github.com>
2021-03-09 16:34:53 +0100

    Delete ein neues testpython dokument.py

--- Python/ein neues testpython dokument.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.linspace(-20,20,1000)
-y = x**3 + 5*x**2 + 10*x + 1
-
-# na hier fehlte aber noch ein cleverer Kommentar!
-
-plt.plot(x,y)

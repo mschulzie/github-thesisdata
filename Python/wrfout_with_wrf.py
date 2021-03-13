7f611b3ec70286ac5716987f4eb11eb6111f7aae
Handballgott <m.schulzie@googlemail.com>
2021-03-13 15:48:47 +0100

    speichern

--- Python/wrfout_with_wrf.py
+++ Python/wrfout_with_wrf.py
@@ -6,7 +6,7 @@ import numpy as np
 from cartopy.feature import NaturalEarthFeature
 
 from wrf import (to_np, getvar, get_cartopy, cartopy_xlim,
-                 cartopy_ylim, latlon_coords)
+                 cartopy_ylim, latlon_coords, ALL_TIMES)
 
 ncfile = Dataset("D://thesisdata/wrf_dust/neu Sven/wrfout_d01_2009-09-18_00_00_00")
 varname = "DUST_SOILFEDRYDEP_ACC_1"
@@ -14,47 +14,48 @@ varname = "DUST_SOILFEDRYDEP_ACC_1"
 var.coords
 
 #i = 40
-for i in range(ncfile.dimensions['Time'].size): # dann plt.show() rausnehmen
+#for i in range(ncfile.dimensions['Time'].size): # dann plt.show() rausnehmen
 
-    var = getvar(ncfile,varname, timeidx=i)
-    lats, lons = latlon_coords(var)
-    # Get the cartopy mapping object
-    cart_proj = get_cartopy(var)
+var = getvar(ncfile,varname, timeidx=ALL_TIMES)
+lats, lons = latlon_coords(var)
+# Get the cartopy mapping object
+cart_proj = get_cartopy(var)
 
-    fig = plt.figure(figsize=(12,6))
-    # Set the GeoAxes to the projection used by WRF
-    ax = plt.axes(projection=cart_proj)
+fig = plt.figure(figsize=(12,6))
+# Set the GeoAxes to the projection used by WRF
+ax = plt.axes(projection=cart_proj)
 
-    # Download and add the states and coastlines
-    states = NaturalEarthFeature(category="cultural", scale="50m",
-                                 facecolor="none",
-                                 name="admin_1_states_provinces_shp")
-    ax.add_feature(states, linewidth=.5, edgecolor="black")
-    ax.coastlines('50m', linewidth=0.8)
+# Download and add the states and coastlines
+states = NaturalEarthFeature(category="cultural", scale="50m",
+                             facecolor="none",
+                             name="admin_1_states_provinces_shp")
+ax.add_feature(states, linewidth=.5, edgecolor="black")
+ax.coastlines('50m', linewidth=0.8)
 
-    # Make the contour outlines and filled contours for the smoothed sea level
-    # pressure.
-    plt.contourf(to_np(lons), to_np(lats), to_np(var),
-                 transform=crs.PlateCarree(),
-                 cmap=get_cmap("viridis"))
-    # Add a color bar
-    plt.colorbar(ax=ax, shrink=.98, label=(var.description+' in '+var.units))
+# Make the contour outlines and filled contours for the smoothed sea level
+# pressure.
 
-    # Set the map bounds
-    ax.set_xlim(cartopy_xlim(var))
-    ax.set_ylim(cartopy_ylim(var))
+plt.contourf(to_np(lons), to_np(lats), to_np(var.sel(Time='2009-09-19T00')),
+             transform=crs.PlateCarree(),
+             cmap=get_cmap("viridis"))
+# Add a color bar
+plt.colorbar(ax=ax, shrink=.98, label=(var.description+' in '+var.units))
 
-    # Add the gridlines
-    ax.gridlines(color="black", linestyle="dotted")
+# Set the map bounds
+ax.set_xlim(cartopy_xlim(var))
+ax.set_ylim(cartopy_ylim(var))
 
-    plt.title(str(var.coords['Time'].values)[:13])
+# Add the gridlines
+ax.gridlines(color="black", linestyle="dotted")
 
-    fig.savefig(
-                'D://thesisdata/bilder/Python/wrfout/'+varname+'/'
-                +str(var.coords['Time'].values)[:13]+'.png', dpi = 300
-                )
-    plt.close()
-    #plt.show()
+plt.title(str(var.coords['Time'].values)[:13])
+
+fig.savefig(
+            'D://thesisdata/bilder/Python/wrfout/'+varname+'/'
+            +str(var.coords['Time'].values)[:13]+'.png', dpi = 300
+            )
+plt.show()
+plt.close()
 
 #SHAO's Variablenvorschläge:
 #

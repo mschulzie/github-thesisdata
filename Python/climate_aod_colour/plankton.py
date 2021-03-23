665c027ab1ce6f8ad56f6192f506124acc8741bb
Handballgott <m.schulzie@googlemail.com>
2021-03-23 19:59:06 +0100

    sliced monthly plankton data to Australia

--- Python/climate_aod_colour/plankton.py
+++ Python/climate_aod_colour/plankton.py
@@ -3,13 +3,10 @@ import os
 import pandas as pd
 import numpy as np
 
-file = '2007_2020_GMIS_A_CHLA.nc'
+file = '2007_2020_GMIS_A_CHLA_Australia.nc'
 path = 'D://thesisdata/plankton/monthly/'
 
 ds = xr.open_dataset(path+file)
+ds = ds['Chl_a']
 
-ds = ds.assign_coords(lon=np.mod(ds['lon'], 360))
-ds = ds.sortby('lon')
-ds = ds.sel(lon=slice(110,190),lat=slice(-9,-60))
-
-ds.to_netcdf(path+'Australia.nc')
+ds.sel(time='2009-10-01').plot()

7931a324f97dbe90e6695f1ff103f12f6c32a972
Handballgott <m.schulzie@googlemail.com>
2021-05-02 14:53:44 +0200

    EOF

--- Python/climate_aod_colour/plankton_daily.py
+++ Python/climate_aod_colour/plankton_daily.py
@@ -2,33 +2,29 @@ import xarray as xr
 import numpy as np
 from xmca.xarray import xMCA
 import pandas as pd
-import marcowhereareyou as mway
+import helperlies as mway
+import matplotlib.pyplot as plt
 
-path = 'D://thesisdata/plankton/cds_daily_2009/'
-file = '2009_08-11_Australia.nc'
-
+path = 'D://thesisdata/plankton/marine_copernicus/'
+file = '2009_local.nc'
 pl = xr.open_dataset(path+file)
-pl = pl['chlor_a']
-pl = pl.sel(lon=slice(110,179))#,lat=slice(-30,-60))
-mway.show_nan(pl)
-
+pl = pl.sel(lon=slice(110,179),lat=slice(-10,-60))
+pl = pl['CHL']
+pl.coords
 #%%
-
+pl = pl.sel(time=slice('2009-08-01','2009-10-31'))
 pl = pl.coarsen(lon=20,lat=20,boundary='trim').mean()
-pl = pl.interpolate_na(dim='time')
-pl = np.log10(pl)
-pl = pl.sel(time=slice('2009-09-01','2009-10-31'))
+#pl = pl.interpolate_na(dim='time')
 
 #%%
 pca = xMCA(pl)
 pca.normalize()
-pca.set_field_names('log10(CHL_A)')
-pca.solve(complexify=True)
-
+pca.set_field_names('CHL_A')
+pca.solve(complexify=False)
 eigenvalues=pca.singular_values()
 pcs = pca.pcs()
 eofs = pca.eofs()
 
-pca.plot(mode=3,threshold=0.5)
-#pca.save_plot(mode=5,path=path)
+pca.plot(mode=1)
+pca.save_plot(mode=2,path='C://Users/mschu/Documents/Studium/Bachelorarbeit/BACHSEM/bilder')
 #pca.save_analysis(path=path)

7eb856c61620ab1ccbdf8c78086c14d8ce3185df
Handballgott <m.schulzie@googlemail.com>
2021-05-28 14:39:11 +0200

    ano

--- Python/chlorophyll/CHL_2009_anomaly.py
+++ Python/chlorophyll/CHL_2009_anomaly.py
@@ -12,19 +12,20 @@ from matplotlib.colors import SymLogNorm, LogNorm
 from cartopy.mpl.ticker import (LongitudeFormatter, LatitudeFormatter,
                                 LatitudeLocator)
 
-cli = xr.open_mfdataset("D://thesisdata/plankton/marine_copernicus/climatology/*.nc")
-ano = xr.open_mfdataset("D://thesisdata/plankton/marine_copernicus/2009/*.nc")
-cli = cli.assign_coords(lon=(cli.lon % 360)).roll(lon=(cli.dims['lon'] // 2), roll_coords=True)
-ano = ano.assign_coords(lon=(ano.lon % 360)).roll(lon=(ano.dims['lon'] // 2), roll_coords=True)
-cli = cli['CHL_mean']
-ano = ano['CHL']
-cli = cli.sel(lon=slice(80,250),lat=slice(-10,-70))
-ano = ano.sel(lon=slice(80,250),lat=slice(-10,-70))
+climate = xr.open_mfdataset("D://thesisdata/plankton/marine_copernicus/climatology/*.nc")
+dawn = xr.open_mfdataset("D://thesisdata/plankton/marine_copernicus/2009/*.nc")
+climate = climate.assign_coords(lon=(climate.lon % 360)).roll(lon=(climate.dims['lon'] // 2), roll_coords=True)
+dawn = dawn.assign_coords(lon=(dawn.lon % 360)).roll(lon=(dawn.dims['lon'] // 2), roll_coords=True)
+climate = climate.sel(lon=slice(80,250),lat=slice(-10,-70))
+dawn = dawn.sel(lon=slice(80,250),lat=slice(-10,-70))
+cli = climate['CHL_mean']
+dev = climate['CHL_standard_deviation']
+ano = dawn['CHL']
 extent = [cli.lon.min(),cli.lon.max(),cli.lat.min(),cli.lat.max()]
 sns.set_context('paper')
 #%%
-for month in range(10,11):
-    for day in range(18,31):
+for month in range(9,10):
+    for day in range(23,24):
 
         time_cli = '1998-'+str(month).zfill(2)+'-'+str(day).zfill(2)
         time = '2009-'+str(month).zfill(2)+'-'+str(day).zfill(2)
@@ -57,12 +58,12 @@ for month in range(10,11):
             ax=ax2,norm=LogNorm(vmax=1),cmap='RdBu', levels=20,
             transform=ccrs.PlateCarree())
         ax3 = fig.add_subplot(3,1,3, projection=ccrs.Mercator(central_longitude=150.0))
-        cli.sel(time=time_cli).squeeze().plot(
-            ax=ax3,norm=LogNorm(vmax=1),cmap='RdBu',levels=20,
+        dev.sel(time=time_cli).squeeze().plot(
+            ax=ax3,norm=SymLogNorm(0.01,base=10),cmap='RdBu',levels=20,
             transform=ccrs.PlateCarree())
         ax2.set_extent(extent, crs=ccrs.PlateCarree())
         ax3.set_extent(extent, crs=ccrs.PlateCarree())
-        #plt.show()
+        plt.show()
         path = 'D://thesisdata/bilder/python/chl_a/auswertung/'
-        fig.savefig(path+str(time)[:10]+'.png', dpi = 500)
-        plt.close()
+        #fig.savefig(path+str(time)[:10]+'.png', dpi = 500)
+        #plt.close()
